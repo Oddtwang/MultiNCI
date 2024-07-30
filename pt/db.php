59ecb7384a5dd34e383168ac93dcf670abe34599
Oddtwang <34981114+Oddtwang@users.noreply.github.com>
2024-07-30 11:23:00 +0100

    Update db.php

--- pt/db.php
+++ pt/db.php
@@ -1,6 +1,6 @@
 <?php
 $pdo = new PDO('mysql:host=localhost;port=8889;dbname=nctti',
-'nctti_web_user', 'EagerB3averPandaCar');
+'nctti_web_user', 'PASSWORD');
 // See the "errors" folder for details...
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
